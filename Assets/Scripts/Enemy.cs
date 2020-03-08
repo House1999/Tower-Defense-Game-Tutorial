485544a71f90c3f93592084ef3cbcd754bb803ce
House1999 <houssein.ballouk.hb2@gmail.com>
2020-03-08 18:38:39 +0100

    I added the current files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0]; // target est le premier waypoint
+    }
+
+    void Update() // every frame
+    {
+        // vecteur x y z pour trouver la direction vers le target
+
+        Vector3 dir = target.position - transform.position; // aller de A a B faut soustraire leur position. ici on va de notre current location au point target
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World); // .normalized comme ca on peut avoir la meme vitesse pour tout (qui sera controller par notre variable speed), deltaTime pour pas dependre des frames mais le temps depuis dernier frame
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)  // if waypoint is reached (within 0.2 unites pour surete)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if(waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);                                // l'ennemie est mort (arrive a la fin)
+            return;                                           // car destroy prend du temps, et visual studio peut lancer les instructions below avant de detruire --> out of range
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex]; // on change de waypoint quand on arrive au waypoint (Donc si on arrive au premier, il le guide vers le 2e)
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount]; // childCount = 13 = nb d'objets dans "waypoints" donc nb de waypoints --> on cree 13 espaces dans l'array
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i); // les rajouter dans notre array points
+        }
+    }
+}
